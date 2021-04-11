c2d4aea035f87f9688b9dde892a5f6e83c8e6042
gabriel-teopes <sysdev.elink@gmail.com>
2021-04-11 19:26:35 +0800

    updates

--- index.php
+++ index.php
@@ -14,23 +14,23 @@
                     <table id="main-table" class="table table-bordered">
                         <tr>
                             <td class="right bold" colspan="5">Discount</td>
-                            <td class="right" style="min-width: 100px;"><input type="text" maxlength="3" class="w-60px">&nbsp;%</td>
+                            <td class="right" style="min-width: 100px;"><input id="discount_com" type="text" maxlength="3" class="w-60px">&nbsp;%</td>
                         </tr>
                         <tr>
                             <td colspan="6">&nbsp;</td>
                         </tr>
                         <tr>
                             <td class="solid s-blue text-white bg-primary center bold" colspan="3">Actual Price</td>
-                            <td id="actual-clmn" colspan="3"></td>
+                            <td colspan="3"></td>
                         </tr>
                         <tr>
-                            <td class="border border-primary" rowspan="5" colspan="3"></td>
-                            <td></td>
+                            <td id="actual-clmn" class="border border-primary bold center middle" rowspan="5" colspan="3"></td>
+                            <td><input type="hidden" id="gross_hidden_val" value="0"></td>
                             <td class="bg-primary text-white right bold" style="min-width: 100px;">Gross</td>
                             <td id="gross-clmn" class="right bold">$0.00</td>
                         </tr>
                         <tr>
-                            <td></td>
+                            <td><input id="partial_hidden_txt" type="hidden" value="0"></td>
                             <td class="bg-primary text-white right bold">Partial Net</td>
                             <td id="partial-clmn" class="right bold">$0.00</td>
                         </tr>
@@ -65,9 +65,9 @@
                                 <td class="center">Product 1</td>
                                 <td id="init-select" class="center"></td>
                                 <td class="center"><input id="quan-1" type="text" class="quan-check w-60px"></td>
-                                <td class="gross-com right"></td>
+                                <td id="gross-com-1" class="gross-com right" data-com_val_gross="0"></td>
                                 <td class="net-com right"></td>
-                                <td class="price-com right"></td>
+                                <td id="price_netcom_1" class="price-com right" data-com_priceNetCom="0"></td>
                             </tr>
                         </tbody>
                         <tfoot>
@@ -85,7 +85,10 @@
         </div>
         <!--
             https://jonsuh.com/blog/javascript-templating-without-a-library/
-            For JS Templating
+            For Native JS Templating
+        
+            https://www.w3resource.com/javascript-exercises/javascript-math-exercise-39.php
+            Math Exercise
         -->
         <script src="/media/js/bootstrap.js"></script>
         <script src="/media/js/jquery-3.6.0.js"></script>
@@ -96,14 +99,14 @@
                 <td class="center">Product {{id}}</td>
                 <td id="col-ctr-{{id}}" class="center"></td>
                 <td class="center"><input id="quan-{{id}}" type="text" class="quan-check w-60px"></td>
-                <td class="gross-com right"></td>
-                <td class="net-com right"></td>
-                <td class="price-com right"></td>
+                <td id="gross-com-{{id}}" class="right gross-com" data-com_val_gross="0"></td>
+                <td class="right net-com"></td>
+                <td id="price_netcom_{{id}}" class="right price-com" data-com_priceNetCom="0"></td>
             </tr>
         </script>
         
         <script id="product-row-scr" type="text/template">
-            <select id="select-{{id}}" class="form-control select2">
+            <select id="select-{{id}}">
                 <option value="0" data-net="0">Select Product</option>
             <?php 
                 include_once 'lib/conn.php';
@@ -140,6 +143,10 @@
                 initSelect();
             });
             
+            $("#discount_com").on("keyup",function(){
+                ComputeTotalNet()
+            });
+            
             function computePrice(obj){
                 var row = $(obj).closest('td').closest('tr').data('row');
                 var quan = $("#quan-" + row).val();
@@ -148,9 +155,29 @@
                 var net = $("#select-" + row).find(":selected").data("net")
                 var price = net * quan;
                 
+                $("#gross-com-" + row).attr('data-com_val_gross',gross_total);
+                $("#price_netcom_" + row).attr('data-com_priceNetCom',price);
+                //console.log('#gross-com-' + row + " = " + $("#gross-com-" + row).attr('data-com_val_gross'));
                 $("#row-" + row + " .gross-com").html(gross_total.toFixed(2));
                 $("#row-" + row + " .net-com").html(net);
                 $("#row-" + row + " .price-com").html(price.toFixed(2));
+                
+                var i = 0;
+                var final_gross = 0;
+                var partial_net = 0;
+                
+                for(i = 1; i <= num_count; i++){
+                    //console.log('data val ' + $("#gross-com-" + i).attr('data-com_val_gross'));
+                    //console.log(i);
+                    final_gross = parseFloat(final_gross) + parseFloat($("#gross-com-" + i).attr('data-com_val_gross'));
+                    partial_net = parseFloat(partial_net) + parseFloat($("#price_netcom_" + i).attr('data-com_priceNetCom'));
+                }
+                $("#partial_hidden_txt").val(partial_net);
+                $("#gross_hidden_val").val(final_gross);
+                //console.log('final_gross ' + final_gross);
+                $("#gross-clmn").html(final_gross.toFixed(2));
+                $("#partial-clmn").html(partial_net.toFixed(2));
+                ComputeTotalNet()
             }
             
             function initSelect(){
@@ -163,6 +190,30 @@
                 });
             }
             
+            function ComputeTotalNet(){
+                var partial = $("#partial_hidden_txt").val() ? parseFloat($("#partial_hidden_txt").val()) : 0;
+                var discount = $("#discount_com").val() ? parseFloat($("#discount_com").val()) : 0;
+                var gross = $("#gross_hidden_val").val() ? parseFloat($("#gross_hidden_val").val()) : 0; 
+                var total_net = 0;
+                var savings = 0;
+                var percentage =0;
+                
+                //console.log("partial " + partial);
+                total_net = partial - (partial * (discount / 100));
+                savings = gross - total_net;
+                percentage = savings > 0 ? savings/total_net * 100 : 0;
+                $("#total-clmn").html(total_net.toFixed(2));
+                $("#actual-clmn").html("$" + numFormat(total_net.toFixed(2)));
+                $("#savings-clmn").html(savings.toFixed(2));
+                $("#percent-clmn").html(percentage.toFixed(1) + "%");
+            }
+            
+            function numFormat(num){
+                var num_parts = num.toString().split(".");
+
+                num_parts[0] = num_parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+                return num_parts.join(".");
+            }
         </script>
     </body>
 </html>
