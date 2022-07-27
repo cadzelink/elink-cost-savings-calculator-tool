01ba2e95d146dc6add61333c1f92599abe8201d0
June Cadayona <junecadayona@elink.corp>
2022-07-27 23:56:30 +0800

    Create login user interface

--- user/routes/web.php
+++ user/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthenticationController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::middleware(['guest'])->group(function () {
+    Route::get('/login', [AuthenticationController::class, 'index'])->name('auth.login');
 });
