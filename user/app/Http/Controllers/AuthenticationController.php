8328774311b87a66523ba1711a4806fb9dbd7e76
Ferlyn Ruiz <ferlynruiz@elink.corp>
2022-07-28 00:22:22 +0800

    Add login functionality

--- user/app/Http/Controllers/AuthenticationController.php
+++ user/app/Http/Controllers/AuthenticationController.php
@@ -3,11 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthenticationController extends Controller
 {
     public function index()
     {
         return view('auth.login');
+    }
+
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return redirect(route('dashboard'));
+        }
+
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records.',
+        ]);
     }
 }
